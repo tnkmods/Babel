8b9b196f0b568927d2f11ee527b40aece5dbbdd4
Nathaniel Kirby <nkirby.ps@gmail.com>
2021-12-26 16:47:22 -0500

    PhantomBlockBehavior

--- src/main/java/com/thenatekirby/babel/block/PhantomBlockBehavior.java
+++ src/main/java/com/thenatekirby/babel/block/PhantomBlockBehavior.java
@@ -1,2 +1,28 @@
-package com.thenatekirby.babel.block;public class PhantomBlockBehavior {
+package com.thenatekirby.babel.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.EntityCollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import javax.annotation.Nonnull;
+
+public class PhantomBlockBehavior {
+    public static VoxelShape getCollisionShape(@Nonnull BlockState blockState, @Nonnull BlockGetter block, @Nonnull BlockPos blockPos, @Nonnull CollisionContext context) {
+        if (!(context instanceof EntityCollisionContext)) {
+            return blockState.getShape(block, blockPos);
+        }
+
+        Entity entity = ((EntityCollisionContext) context).getEntity();
+        if (entity instanceof Player) {
+            return Shapes.empty();
+        } else {
+            return blockState.getShape(block, blockPos);
+        }
+    }
 }
