828f8313be77f0e8ef86b3b3d1bf8508b9eb3d7d
Nathaniel Kirby <nkirby.ps@gmail.com>
2021-12-26 16:47:36 -0500

    Work towards JEPB

--- src/main/java/com/thenatekirby/babel/mixin/UniformGeneratorMixin.java
+++ src/main/java/com/thenatekirby/babel/mixin/UniformGeneratorMixin.java
@@ -1,2 +1,17 @@
-package com.thenatekirby.babel.mixin;public class UniformGeneratorMixin {
+package com.thenatekirby.babel.mixin;
+
+import net.minecraft.world.level.storage.loot.providers.number.NumberProvider;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+// ====---------------------------------------------------------------------------====
+
+@Mixin(UniformGenerator.class)
+public interface UniformGeneratorMixin {
+    @Accessor("min")
+    NumberProvider getUniformMin();
+
+    @Accessor("max")
+    NumberProvider getUniformMax();
 }
--- src/main/java/com/thenatekirby/babel/util/loot/LootRange.java
+++ src/main/java/com/thenatekirby/babel/util/loot/LootRange.java
@@ -1,2 +1,40 @@
-package com.thenatekirby.babel.util.loot;public class LootRange {
+package com.thenatekirby.babel.util.loot;
+
+import com.thenatekirby.babel.mixin.ConstantValueMixin;
+import com.thenatekirby.babel.mixin.UniformGeneratorMixin;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.NumberProvider;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+
+public class LootRange {
+    public static final LootRange ZERO = new LootRange(0, 0);
+    private final float min;
+    private final float max;
+
+    public LootRange(float min, float max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    public float getMin() {
+        return min;
+    }
+
+    public float getMax() {
+        return max;
+    }
+
+    public static LootRange from(UniformGenerator generator) {
+        NumberProvider minProvider = ((UniformGeneratorMixin) generator).getUniformMin();
+        NumberProvider maxProvider = ((UniformGeneratorMixin) generator).getUniformMax();
+        if (!(minProvider instanceof ConstantValue minValue) || !(maxProvider instanceof ConstantValue maxValue)) {
+            return ZERO;
+        }
+
+        float min = ((ConstantValueMixin) (Object) minValue).getValue();
+        float max = ((ConstantValueMixin) (Object) maxValue).getValue();
+
+        return new LootRange(min, max);
+    }
 }
