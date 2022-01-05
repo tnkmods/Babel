fa66beb37b22c4c27de22ab8f269cb239e049bc7
Nathaniel Kirby <nkirby.ps@gmail.com>
2022-01-04 20:39:20 -0500

    Core: Warning Cleanup on BabelMod

--- src/main/java/com/thenatekirby/babel/core/BabelMod.java
+++ src/main/java/com/thenatekirby/babel/core/BabelMod.java
@@ -46,28 +46,22 @@ public class BabelMod {
     }
 
     private void onClientSetup(FMLClientSetupEvent event) {
-        lifecycleAdapter.ifPresent(adapter -> {
-            event.enqueueWork(() -> {
-                adapter.onClientSetup(event);
-                adapter.onRegisterScreens();
-            });
-        });
+        lifecycleAdapter.ifPresent(adapter -> event.enqueueWork(() -> {
+            adapter.onClientSetup(event);
+            adapter.onRegisterScreens();
+        }));
     }
 
     private void onCommonSetup(FMLCommonSetupEvent event) {
-        lifecycleAdapter.ifPresent(adapter -> {
-            event.enqueueWork(() -> {
-                adapter.onCommonSetup(event);
-                adapter.onRegisterPackets();
-            });
-        });
+        lifecycleAdapter.ifPresent(adapter -> event.enqueueWork(() -> {
+            adapter.onCommonSetup(event);
+            adapter.onRegisterPackets();
+        }));
     }
 
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-        lifecycleAdapter.ifPresent(adapter -> {
-            adapter.onServerStarting(event);
-        });
+        lifecycleAdapter.ifPresent(adapter -> adapter.onServerStarting(event));
     }
 
     @SubscribeEvent
