bdf933cddb39d1f1345372a4b45262e782e26d7b
Nathaniel Kirby <nkirby.ps@gmail.com>
2022-01-06 09:12:24 -0500

    Loot: Add accessor for getting potion from loot function

--- src/main/java/com/thenatekirby/babel/mixin/SetPotionFunctionMixin.java
+++ src/main/java/com/thenatekirby/babel/mixin/SetPotionFunctionMixin.java
@@ -1,2 +1,14 @@
-package com.thenatekirby.babel.mixin;public class SetPotionFunctionMixin {
+package com.thenatekirby.babel.mixin;
+
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.level.storage.loot.functions.SetPotionFunction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+// ====---------------------------------------------------------------------------====
+
+@Mixin(SetPotionFunction.class)
+public interface SetPotionFunctionMixin {
+    @Accessor("potion")
+    Potion getPotion();
 }
--- src/main/java/com/thenatekirby/babel/util/loot/LootFunctionUtil.java
+++ src/main/java/com/thenatekirby/babel/util/loot/LootFunctionUtil.java
@@ -3,11 +3,14 @@ package com.thenatekirby.babel.util.loot;
 import com.thenatekirby.babel.mixin.EnchantRandomlyFunctionMixin;
 import com.thenatekirby.babel.mixin.SetItemCountFunctionMixin;
 import com.thenatekirby.babel.mixin.SetNbtFunctionMixin;
+import com.thenatekirby.babel.mixin.SetPotionFunctionMixin;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.storage.loot.functions.EnchantRandomlyFunction;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.functions.SetNbtFunction;
+import net.minecraft.world.level.storage.loot.functions.SetPotionFunction;
 import net.minecraft.world.level.storage.loot.providers.number.NumberProvider;
 
 import javax.annotation.Nonnull;
@@ -29,5 +32,10 @@ public class LootFunctionUtil {
     @Nonnull
     public static NumberProvider getValue(@Nonnull SetItemCountFunction function) {
         return ((SetItemCountFunctionMixin) function).getValue();
+    }
+
+    @Nonnull
+    public static Potion getPotion(@Nonnull SetPotionFunction function) {
+        return ((SetPotionFunctionMixin) function).getPotion();
     }
 }
